07bf3c13d2197ea42791e26aec7486e651d1a7c5
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-11-12 00:44:21 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,10 +1,10 @@
 export const contactsData = {
     email: 'er.adilrasheed@gmail.com',
     phone: '+919616316925',
-    address: 'Mau,Uttar Pradesh,India',
+    address: 'Hyderabad, Telangana,India',
     github: 'https://github.com/eradilrasheed',
     facebook: 'https://www.facebook.com/Er.MARasheed/',
-    linkedIn: 'https://www.linkedin.com/in/adilrasheedknit/',
+    linkedIn: 'https://www.linkedin.com/in/madrashee/',
     twitter: 'https://twitter.com/er_marasheed',
     stackOverflow: 'https://stackoverflow.com/users/23922646/mohammad-adil-rasheed',
     devUsername: "adil4474"
