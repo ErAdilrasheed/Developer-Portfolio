8a5b72a6261d4e3e46029fc084482c0d42dbad87
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-11-12 00:50:52 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Master of Computer Application",
-    duration: "2022 - Present",
+    duration: "Aug 2022 - Jun 2024",
     institution: "Kamla Nehru Institute of Technology Sultanpur",
   },
   {
     id: 2,
     title: "Bachelor of Computer Application",
-    duration: "2016 - 2019",
+    duration: "July 2016 - Jun 2019",
     institution: "Khwaja Moinuddin Chishti Language University Lucknow",
   },
   {
     id: 3,
-    title: "Higher Secondary School",
-    duration: "2014 - 2016",
-    institution: "Talimuddin Inter College Mau",
+    title: "Bachelor of Art - Geography",
+    duration: "Aug 2013 - Jun 2016",
+    institution: "Veer Bahadur Singh Purvanchal University Jaunpur",
   },
 ]
